dffb3341c5005925807d93524be6ce018ad81528
SylwiaNG <iwon9@poczta.onet.pl>
2024-05-19 19:44:29 +0200

    make build

---
+++ index.46ccbe3d.js
@@ -0,0 +1,2 @@
+function r(r){return Number(r.replace(/[^0-9.-]+/g,""))}Array.from(document.querySelector("ul").children).sort(function(e,a){var t=r(e.dataset.salary);return r(a.dataset.salary)-t});
+//# sourceMappingURL=index.46ccbe3d.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+function convertSalaryToNumber(salaryString) {
+    return Number(salaryString.replace(/[^0-9.-]+/g, ""));
+}
+const list = document.querySelector("ul");
+const items = Array.from(list.children);
+items.sort((a, b)=>{
+    const salaryA = convertSalaryToNumber(a.dataset.salary);
+    const salaryB = convertSalaryToNumber(b.dataset.salary);
+    return salaryB - salaryA;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
